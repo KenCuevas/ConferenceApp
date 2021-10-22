45aada898aa33c21316028137f3dc55ceee7ed34
kcuevas99 <k3cuevas@protonmail.com>
2021-10-21 23:06:30 -0400

    Agregado el Jar

--- src/test/java/com/ihatemycode/ConferenceAppApplicationTests.java
+++ src/test/java/com/ihatemycode/ConferenceAppApplicationTests.java
@@ -1,11 +1,14 @@
 package com.ihatemycode;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConferenceAppApplicationTests {
-
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceAppApplication.class, args);
+	}
 	@Test
 	void contextLoads() {
 	}
